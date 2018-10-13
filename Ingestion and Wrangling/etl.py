729c31d7da201e0f178b61fc51587a09fb5e9af3
Won <wjchoe83@gmail.com>
2018-10-13 14:04:54 -0400

    Added folder

---
+++ Ingestion and Wrangling/etl.py
@@ -0,0 +1,12 @@
+import requests
+import zipfile
+
+# download the Kiva snapshot in csv format to local directory
+url = "http://s3.kiva.org/snapshots/kiva_ds_csv.zip"
+download = requests.get(url)
+open('kiva_ds_csv.zip', 'wb').write(r.content)
+
+# unzip the file to local directory
+kiva_csv = zipfile.ZipFile('kiva_ds_csv.zip', 'r')
+kiva_csv.extractall()
+kiva_csv.close()

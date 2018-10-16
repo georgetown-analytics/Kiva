1c8d95f27387e8655b5ebe9ed9372913e30908d7
Won <wjchoe83@gmail.com>
2018-10-16 10:29:01 -0400

    Fixed download code and tested

--- Ingestion and Wrangling/etl.py
+++ Ingestion and Wrangling/etl.py
@@ -3,8 +3,8 @@ import zipfile
 
 # download the Kiva snapshot in csv format to local directory
 url = "http://s3.kiva.org/snapshots/kiva_ds_csv.zip"
-download = requests.get(url)
-open('kiva_ds_csv.zip', 'wb').write(r.content)
+download = requests.get(url, allow_redirects=True)
+open('kiva_ds_csv.zip', 'wb').write(download.content)
 
 # unzip the file to local directory
 kiva_csv = zipfile.ZipFile('kiva_ds_csv.zip', 'r')
